cc5e820d59860e11985134fa9caadaa126a624df
ProudPurple <andersonkimball5@gmail.com>
2025-09-21 11:47:36 -0400

    VSCode Internal Setup Complete

--- run/virtual-pet.cpp
+++ run/virtual-pet.cpp
@@ -1,9 +1,18 @@
 #include <SFML/Graphics.hpp>
-
+using namespace sf;
+using namespace std;
 int main() {
-    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(400, 300)), "Test Window");
+    RenderWindow window(VideoMode(Vector2u(600, 400)), "Test Window", Style::None, State::Windowed);
+    window.setFramerateLimit(60);
+    window.setPosition({0,0});
     while (window.isOpen()) {
-        window.clear(sf::Color::Red);
+        window.clear(Color::Red);
         window.display();
+        while (const optional event = window.pollEvent()) {
+            if (event->is<Event::Closed>())
+                window.close();
+            if (event->is<Event::MouseButtonPressed>())
+                window.clear(Color::Blue);
+        }
     }
 }
