2912d2971a94f7a73f66fb1e022c8152ec20fc42
ProudPurple <andersonkimball5@gmail.com>
2025-09-24 18:00:30 -0400

    Transparent Backgrounds and File Saving

--- README.txt
+++ README.txt
@@ -1,13 +1,8 @@
 This is my fbla project
 
-/c/Users/ander/OneDrive/Desktop/Work/Coding/FBLA-Project/Virtual-Pet
-C:\Users\ander\OneDrive\Desktop\Work\Coding\FBLA-Project\Virtual-Pet
-TO COMPILE OPEN x64 Native blah blah blah
-change cd or whatever to the project folder then run
-run cl /EHsc /std:c++17 virtual-pet.cpp /I C:\Users\ander\SFML-3.0.2\include /link /LIBPATH:C:\Users\ander\SFML-3.0.2\lib sfml-graphics.lib sfml-window.lib sfml-system.lib
-woohoo compiled
+To run/compile just press f5
 
-git needs git clang whatever
+to save and stuff open a gitbash terminal in vscode
 first git status to see what needed
 to add file to git run git add file_name
 to add all type git add .
--- run/virtual-pet.cpp
+++ run/virtual-pet.cpp
@@ -1,18 +1,61 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <Windows.h>
+#include <iostream>
+#include <fstream>
 using namespace sf;
 using namespace std;
+
+struct Stats {
+    int hunger;
+    int mood;
+    int money;
+};
+
+void save(Stats stats) {
+    ofstream fout("stats.txt");
+    fout << stats.hunger << endl
+    << stats.mood << endl
+    << stats.money << endl;
+}
+
 int main() {
-    RenderWindow window(VideoMode(Vector2u(600, 400)), "Test Window", Style::None, State::Windowed);
+    ifstream fin("stats.txt");
+    Stats stats;
+    fin >> stats.hunger >> stats.mood >> stats.money;
+    cout << stats.hunger << ' ' << stats.mood << ' ' << stats.money;
+
+    RenderWindow window(VideoMode(Vector2u(600, 400)), "Virtual Pet", Style::None, State::Windowed);
+
     window.setFramerateLimit(60);
     window.setPosition({0,0});
+
+    HWND hwnd = window.getNativeHandle();
+
+    LONG style = GetWindowLong(hwnd, GWL_EXSTYLE);
+    SetWindowLong(hwnd, GWL_EXSTYLE, style | WS_EX_LAYERED);
+
+    SetLayeredWindowAttributes(hwnd, RGB(0,0,0), 0, LWA_COLORKEY);
+
+    CircleShape circle(100.f);
+    circle.setFillColor(Color::Green);
+
     while (window.isOpen()) {
-        window.clear(Color::Red);
+
+        
+
+        window.clear(Color::Black);
+        window.draw(circle);
         window.display();
         while (const optional event = window.pollEvent()) {
-            if (event->is<Event::Closed>())
+            if (event->is<Event::Closed>()) {
                 window.close();
-            if (event->is<Event::MouseButtonPressed>())
-                window.clear(Color::Blue);
+                save(stats);
+            }
         }
+            
+        
+        if (Keyboard::isKeyPressed(Keyboard::Key::A))
+            stats.hunger--;
     }
 }
