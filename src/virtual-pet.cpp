2fb35366503ac866a9f1e80601614bd2be7e3198
ProudPurple <andersonkimball5@gmail.com>
2025-10-05 11:44:45 -0400

    Displaying Sprites and Text

--- src/virtual-pet.cpp
+++ src/virtual-pet.cpp
@@ -21,29 +21,39 @@ void save(Stats stats) {
 }
 
 int main() {
+    //Takes in saved stats from txt file
     ifstream fin("stats.txt");
     Stats stats;
     fin >> stats.hunger >> stats.mood >> stats.money;
+    //Can copy paste line for in context erroring
     //MessageBox(NULL, to_string(stats.hunger).c_str(), "Debug", MB_OK);
 
+    //Make window and set some basic stuff
     RenderWindow window(VideoMode(Vector2u(600, 400)), "Virtual Pet", Style::None, State::Windowed);
-
     window.setFramerateLimit(60);
     window.setPosition({0,0});
 
+    //Make window background clear
     HWND hwnd = window.getNativeHandle();
-
     LONG style = GetWindowLong(hwnd, GWL_EXSTYLE);
     SetWindowLong(hwnd, GWL_EXSTYLE, style | WS_EX_LAYERED);
-
     SetLayeredWindowAttributes(hwnd, RGB(0,0,0), 0, LWA_COLORKEY);
 
-    CircleShape circle(100.f);
-    circle.setFillColor(Color::Green);
+    Texture texture{"sprites/cat1.png", false, IntRect({-50,-50}, {100,100})};
+    Sprite sprite(texture);
+
+    Font font("bin/RasterForgeRegular-JpBgm.ttf");
+    Text text(font);
+    text.setString(to_string(stats.hunger));
+    text.setCharacterSize(20);
+    text.setFillColor(Color::Green);
+    text.setStyle(Text::Bold);
 
     while (window.isOpen()) {
         window.clear(Color::Black);
-        window.draw(circle);
+        window.draw(sprite);
+        window.draw(text);
+        text.setString(to_string(stats.hunger));
         window.display();
 
         while (const optional event = window.pollEvent()) {
